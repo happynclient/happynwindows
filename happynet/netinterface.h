53c20315b18ccc0f87cb3aa43c23907081838689
happyntec <tec@happyn.cn>
2022-07-12 15:02:56 +0800

    HPYCLIENT-126

--- happynet/netinterface.h
+++ happynet/netinterface.h
@@ -1,6 +1,8 @@
 #ifndef _H_NET_INTERFACE
 #define _H_NET_INTERfACE
 
+
+BOOL set_netinterface_name_by_id(WCHAR device_uuid[]);
 DWORD get_adapter_friendly_name(CHAR* adapter_name, WCHAR* friendly_name, ULONG max_name_length);
 void get_mac_address(WCHAR* mac_address, WCHAR* guid);
 void get_addresses(WCHAR* ip_address, WCHAR* mac_address);
