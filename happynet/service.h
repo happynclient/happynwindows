637bf6a2d2c7ce52d911f6ed5070d4c314da48b0
happyntec <tec@happyn.cn>
2022-09-25 16:44:12 +0800

    for #3

--- happynet/service.h
+++ happynet/service.h
@@ -1,7 +1,6 @@
 #ifndef _H_SERVICE
 #define _H_SERVICE
 
-#define MAX_COMMAND_LINE_LEN 1024*8
 
 DWORD get_service_status();
 void start_service();
@@ -9,7 +8,6 @@ void stop_service();
 void set_auto_start_service();
 void cancel_auto_start_service();
 
-void log_event(WCHAR* format, ...);
 int build_exe_path(WCHAR* exe_path, DWORD buf_len);
 int build_command_line_edge(WCHAR* exe_path, WCHAR* command_line, DWORD buf_len);
 
