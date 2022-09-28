837ce3ecd5563d90df23a2f0f94ec8966129856b
happyntec <tec@happyn.cn>
2022-09-28 17:23:37 +0800

    for #3

--- happynet/systemsrv.h
+++ happynet/systemsrv.h
@@ -1,12 +1,12 @@
 #ifndef _H_SYSTEMSRV
 #define _H_SYSTEMSRV
 
-void reg_service_system(WCHAR* command_line);
-void unreg_service_system(WCHAR* command_line);
-BOOL start_service_system(WCHAR* command_line);
+void reg_service_system(void);
+void unreg_service_system(void);
+BOOL start_service_system(void);
 void grace_stop_service_system(void);
 void terminal_service_system(void);
 DWORD get_service_system_status(void);
-void get_service_system_output(WCHAR *read_buf);
+void get_service_system_output(void);
 
 #endif
