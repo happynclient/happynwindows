a638d30f236647f94f750a822ff7d53b3978072f
happyntec <tec@happyn.cn>
2022-10-02 15:59:05 +0800

    for #7

--- happynet/registry.h
+++ happynet/registry.h
@@ -1,10 +1,10 @@
 #ifndef _H_REGISTRY
 #define _H_REGISTRY
 
-int reg_get_dword(HKEY hkey, LPWSTR value_name, LPDWORD ret_dword);
-int reg_get_string(HKEY hkey, LPWSTR value_name, LPWSTR ret_str, DWORD buf_size);
-int reg_set_dword(HKEY hkey, LPWSTR value_name, DWORD dword_val);
-int reg_set_string(HKEY hkey, LPWSTR value_name, LPWSTR str_val);
+INT reg_get_dword(HKEY hkey, LPWSTR value_name, LPDWORD ret_dword);
+INT reg_get_string(HKEY hkey, LPWSTR value_name, LPWSTR ret_str, DWORD buf_size);
+INT reg_set_dword(HKEY hkey, LPWSTR value_name, DWORD dword_val);
+INT reg_set_string(HKEY hkey, LPWSTR value_name, LPWSTR str_val);
 BOOL is_system_service();
 BOOL is_auto_start();
 BOOL is_auto_tray();
