0f57f0252f65bd317219afddac0dc7f4a802f959
happyntec <tec@happyn.cn>
2022-10-02 16:05:26 +0800

    for #7

--- happynet/service.h
+++ happynet/service.h
@@ -1,10 +1,10 @@
 #ifndef _H_SERVICE
 #define _H_SERVICE
 
-DWORD get_service_status();
-void start_service();
-void stop_service();
-void set_auto_start_service();
-void cancel_auto_start_service();
+DWORD get_service_status(VOID);
+VOID start_service(VOID);
+VOID stop_service(VOID);
+VOID set_auto_start_service(VOID);
+VOID cancel_auto_start_service(VOID);
 
 #endif
