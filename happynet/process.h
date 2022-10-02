4ff1dca34db72bdb4bfc9f366e197dd2e866d581
happyntec <tec@happyn.cn>
2022-10-02 15:31:22 +0800

    for #7

--- happynet/process.h
+++ happynet/process.h
@@ -4,15 +4,14 @@
 #include <windows.h> 
 
 #define PROCESS_STDOUT_BUFSIZE 4096
-
 #define PROCESS_EXIT_CODE	0
 
 HANDLE create_service_process(WCHAR* command_line);
-void terminal_service_process(void);
-void grace_stop_service_process(void);
-DWORD get_service_process_status(void);
-void get_service_process_error(PTSTR); 
-void get_service_process_output(WCHAR *readBuf);
+VOID terminal_service_process(VOID);
+VOID grace_stop_service_process(VOID);
+DWORD get_service_process_status(VOID);
+VOID get_service_process_error(PTSTR); 
+VOID get_service_process_output(WCHAR *readBuf);
 
 
 #endif
