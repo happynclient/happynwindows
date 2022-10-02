747d77c22c2b2a899460c42ee5ba7af5c05e9fc0
happyntec <tec@happyn.cn>
2022-10-02 16:05:26 +0800

    for #7

--- happynet/service.cpp
+++ happynet/service.cpp
@@ -11,9 +11,9 @@
 #include "systemsrv.h"
 #include "utils.h"
 
-HANDLE pid;
+HANDLE m_pid;
 
-DWORD get_service_status()
+DWORD get_service_status(VOID)
 {
 	//if( STILL_ACTIVE == dwMark) //running
 	//if( PROCESS_EXIT_CODE == dwMark) //stopped
@@ -23,15 +23,15 @@ DWORD get_service_status()
 	return get_service_process_status();
 }
 
-void start_service()
+VOID start_service(VOID)
 {
     if (is_system_service()) {
         start_service_system();
         return;
     }
 
-    WCHAR dir_path[MAX_PATH] = { '\0' };
-    WCHAR command_line[MAX_COMMAND_LINE_LEN] = { '\0' };    
+    WCHAR dir_path[MAX_PATH] = { 0 };
+    WCHAR command_line[MAX_COMMAND_LINE_LEN] = { 0 };
 	if (get_service_status() == STILL_ACTIVE) {
 		return;
 	}
@@ -39,23 +39,23 @@ void start_service()
 	// Build path and command line parameters
 	if (!get_install_dir_path(dir_path, MAX_PATH))
 	{
-		log_event(L"%s:%d (%s) - Error building executable path.\n", __FILEW__, __LINE__, __FUNCTIONW__);
+		log_event(TEXT("%s:%d (%s) - Error building executable path.\n"),
+                    __FILEW__, __LINE__, __FUNCTIONW__);
 		return;
 	}
-	int ret = 0;
+	INT ret = 0;
 
 	ret = get_command_line_edge(dir_path, command_line, MAX_COMMAND_LINE_LEN);
 
-	log_event(L"%s:%d (%s) - building command line: %s \n", __FILEW__, __LINE__, __FUNCTIONW__, command_line);
-
-	log_event(L"\n->Start of parent execution.\n");
-
+	log_event(TEXT("%s:%d (%s) - building command line: %s \n"),
+                __FILEW__, __LINE__, __FUNCTIONW__, command_line);
+	log_event(TEXT("\n->Start of parent execution.\n"));
 
 	// Create the child process. 
 	create_service_process(command_line); 
 }
 
-void stop_service()
+VOID stop_service(VOID)
 {
     if (is_system_service()) {
         stop_service_system();
@@ -68,7 +68,7 @@ void stop_service()
 }
 
 // auto start exe when system startup
-void set_auto_start_service()
+VOID set_auto_start_service(VOID)
 {
     if (is_system_service()) {
         set_auto_start_service_system();
@@ -79,7 +79,7 @@ void set_auto_start_service()
 	//std::string strRegPath = "SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run";
 
 	//open system auto run regedit item
-	if (RegOpenKeyEx(HKEY_CURRENT_USER, _T("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run"), 0, KEY_ALL_ACCESS, &hkey) == ERROR_SUCCESS)
+	if (RegOpenKeyEx(HKEY_CURRENT_USER, TEXT("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run"), 0, KEY_ALL_ACCESS, &hkey) == ERROR_SUCCESS)
 	{
 		//get exe path
 		TCHAR str_exe_fulldir[MAX_PATH];
@@ -93,7 +93,7 @@ void set_auto_start_service()
 		if (!reg_get_string(hkey, L"Happynet", ret_val, 512) || _tcscmp(str_exe_fulldir, str_dir) != 0)
 		{
 			//append child Key and set value:"happynet" is exe name
-			RegSetValueEx(hkey, _T("Happynet"), 0, REG_SZ, (LPBYTE)str_exe_fulldir, (lstrlen(str_exe_fulldir) + 1)*sizeof(TCHAR));
+			RegSetValueEx(hkey, TEXT("Happynet"), 0, REG_SZ, (LPBYTE)str_exe_fulldir, (lstrlen(str_exe_fulldir) + 1)*sizeof(TCHAR));
 
 			//close regedit
 			RegCloseKey(hkey);
@@ -103,7 +103,7 @@ void set_auto_start_service()
 
 
 //cancle auto start
-void cancel_auto_start_service()
+VOID cancel_auto_start_service(VOID)
 {
     if (is_system_service()) {
         cancel_auto_start_service_system();
@@ -112,9 +112,9 @@ void cancel_auto_start_service()
     HKEY hkey;
 	//std::string strRegPath = "SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run";
 
-	if (RegOpenKeyEx(HKEY_CURRENT_USER, _T("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run"), 0, KEY_ALL_ACCESS, &hkey) == ERROR_SUCCESS)
+	if (RegOpenKeyEx(HKEY_CURRENT_USER, TEXT("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run"), 0, KEY_ALL_ACCESS, &hkey) == ERROR_SUCCESS)
 	{
-		RegDeleteValue(hkey, _T("Happynet"));
+		RegDeleteValue(hkey, TEXT("Happynet"));
 		RegCloseKey(hkey);
 	}
 }
