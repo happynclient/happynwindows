bb84cd782cb1788eefcff0e105d9caa26dba3ae2
happyntec <tec@happyn.cn>
2022-10-02 17:36:13 +0800

    for #6:process module

--- happynet/process.h
+++ happynet/process.h
@@ -6,12 +6,12 @@
 #define PROCESS_STDOUT_BUFSIZE 4096
 #define PROCESS_EXIT_CODE	0
 
-HANDLE create_service_process(WCHAR* command_line);
-VOID terminal_service_process(VOID);
-VOID grace_stop_service_process(VOID);
-DWORD get_service_process_status(VOID);
-VOID get_service_process_error(PTSTR); 
-VOID get_service_process_output(WCHAR *readBuf);
+HANDLE CreateProcessService(WCHAR* pszCmdLine);
+VOID TerminalProcessService(VOID);
+VOID GraceStopProcessService(VOID);
+DWORD GetProcessServiceStatus(VOID);
+VOID GetProcessServiceError(PTSTR lpszFunction);
+VOID GetProcessServiceOutput(WCHAR *pszReadBuf);
 
 
 #endif
