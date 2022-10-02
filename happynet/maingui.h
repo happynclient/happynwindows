374e7b86b14a507e239eaaf2cd31fb1cd18f695f
happyntec <tec@happyn.cn>
2022-10-02 16:36:15 +0800

    for #7

--- happynet/maingui.h
+++ happynet/maingui.h
@@ -1,18 +1,18 @@
 #ifndef _H_MAINGUI
 #define _H_MAINGUI
 
-bool string_empty(WCHAR* str);
-bool validate_options(HWND hwndDlg);
-void update_addresses(HWND hwndDlg);
-void update_service_status(HWND hwndDlg);
-void CALLBACK refresh_screen(HWND hwndDlg, UINT uMsg, UINT_PTR idEvent, DWORD dwTime);
-void read_options(HWND hwndDlg);
-void save_options(HWND hwndDlg);
-void handle_command_event(HWND hwndDlg, UINT uMsg, WPARAM wParam, LPARAM lParam);
+BOOL string_empty(WCHAR* str);
+BOOL validate_options(HWND hwndDlg);
+VOID update_addresses(HWND hwndDlg);
+VOID update_service_status(HWND hwndDlg);
+VOID CALLBACK refresh_screen(HWND hwndDlg, UINT uMsg, UINT_PTR idEvent, DWORD dwTime);
+VOID read_options(HWND hwndDlg);
+VOID save_options(HWND hwndDlg);
+VOID handle_command_event(HWND hwndDlg, UINT uMsg, WPARAM wParam, LPARAM lParam);
 INT_PTR CALLBACK dialog_proc(HWND hwndDlg, UINT uMsg, WPARAM wParam, LPARAM lParam);
 INT_PTR CALLBACK ad_settings_dialog_proc(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam);
 
-void setup_system_menu(HWND hwndDlg);
+VOID setup_system_menu(HWND hwndDlg);
 int WINAPI WinMain (HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow);
 
 #endif
