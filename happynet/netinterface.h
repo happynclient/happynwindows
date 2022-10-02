dbf01a25750de6acdfe86fd1a793bd0d84200ae8
happyntec <tec@happyn.cn>
2022-10-02 19:25:01 +0800

    for #6:netinterface module

--- happynet/netinterface.h
+++ happynet/netinterface.h
@@ -2,12 +2,12 @@
 #define _H_NET_INTERfACE
 
 
-BOOL set_netinterface_name_by_id(WCHAR device_uuid[]);
-DWORD get_adapter_friendly_name(CHAR* adapter_name, WCHAR* friendly_name, ULONG max_name_length);
-VOID get_mac_address(WCHAR* mac_address, WCHAR* guid);
-VOID get_addresses(WCHAR* ip_address, WCHAR* mac_address);
-BOOL validate_ipv4_address(WCHAR* ip_address);
-BOOL validate_mac_address(WCHAR* mac_address);
+BOOL SetNetinterfaceNameById(WCHAR device_uuid[]);
+DWORD GetAdapterFriendlyName(CHAR* adapter_name, WCHAR* friendly_name, ULONG max_name_length);
+VOID GetMacAddress(WCHAR* mac_address, WCHAR* guid);
+VOID GetIpMacAddresses(WCHAR* ip_address, WCHAR* mac_address);
+BOOL ValidateIpv4Address(WCHAR* ip_address);
+BOOL ValidateMacAddress(WCHAR* mac_address);
 BOOL validate_number_range(WCHAR* num, INT min, INT max);
 
 #endif
